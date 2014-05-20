2c8397927387be783f9417708261aab4cff9d1d0
gstearmit@gmail.com <gstearmit@gmail.com@815b5aec-6364-ffa6-e4c1-eef17ad42f80>
2014-05-20 09:39:32 +0000

    Resol Modul Storydetail Modul Story add two database spint2

---
+++ module/Storydetail/config/module.config.php
@@ -0,0 +1,43 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Storydetail\Controller\Storydetail' => 'Storydetail\Controller\StorydetailController',
+        	'Storydetail\Controller\Upload' => 'Storydetail\Controller\UploadController'
+        ),
+    ),
+
+    'router' => array(
+        'routes' => array(
+            'storydetail' => array(
+                'type'    => 'segment',
+                'options' => array(
+                  //  'route'    => '/storydetail[/][:controller][/][:action][/:id][/page/:page][/order_by/:order_by][/:order][/mz/:idmz]',
+					 'route'    => '/storydetail[/:action][/:id][/page/:page][/order_by/:order_by][/:order][/mz/:idmz]',
+                    'constraints' => array(
+                    	//'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action' => '(?!\bpage\b)(?!\border_by\b)[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                        'page' => '[0-9]+',
+                        'order_by' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'order' => 'ASC|DESC',
+                    ),
+                    'defaults' => array(
+                    	'__NAMESPACE__' => 'Storydetail\Controller',
+                        'controller' => 'Storydetail\Controller\Storydetail',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Storydetail' => __DIR__ . '/../view',
+        ),
+        'template_map' => array(
+            'page-Storydetail' => __DIR__ . '/../view/layout/slidePaginator.phtml',
+        ),
+    ),
+);
---
+++ module/Storydetail/src/Storydetail/Model/StorydetailTable.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Storydetail\Model;
+
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Where;
+
+class StorydetailTable extends AbstractTableGateway {
+
+    protected $table = 'storydetail';
+    
+
+    public function __construct(Adapter $adapter) {
+        $this->adapter = $adapter;
+        $this->resultSetPrototype = new ResultSet();
+        $this->resultSetPrototype->setArrayObjectPrototype(new Storydetail());
+
+        $this->initialize();
+    }
+
+    public function fetchAll(Select $select = null) {
+        if (null === $select)
+        $select = new Select();
+        $select->from($this->table);
+        $resultSet = $this->selectWith($select);
+        $resultSet->buffer();
+        return $resultSet;
+    }
+
+    public function getStorydetail($id) {
+        $id = (int) $id;
+        $rowset = $this->select(array('id' => $id));
+        
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+       
+       
+    }
+    
+    
+    public function getrestStorydetail($id) {
+    	$id = (int) $id;
+    	//         $rowset = $this->select(array('id' => $id));
+    
+    	//         $row = $rowset->current();
+    	//         if (!$row) {
+    	//             throw new \Exception("Could not find row $id");
+    	//         }
+    	//         return $row;
+    	$sql = new Sql($this->adapter);
+    	$select = $sql->select();
+    	//$select->columns(array('id'=>'id','title'=>'title','descriptionkey'=>'descriptionkey','imgkey'=>'imgkey'));
+    	$select->columns(array());
+    	$select->from ('story')
+    	->join('storydetail', 'storydetail.id=story.id',array('id'=>'id','img'=>'img','description'=>'description','title'=>'title','page'=>'page'));
+    	$select->where(array('story.id'=>$id));
+    
+    	$selectString = $sql->prepareStatementForSqlObject($select);
+    	$results = $selectString->execute();
+    	 
+    	// swap
+    	$array = array();
+    	foreach ($results as $result)
+    	{
+    		$tmp = array();
+    		$tmp= $result;
+    		$array[] = $tmp;
+    	}
+    
+    	return $array;
+    	 
+    }
+
+    public function saveStorydetail(Storydetail $storydetail) {
+        $data = array(
+            'idmz' => $storydetail->idmz,
+        	'img' => $storydetail->img,
+            'description' => $storydetail->description,
+			'title' => $storydetail->title,
+			'page' => $storydetail->page,
+        );
+        
+
+        $id = (int) $storydetail->id;
+        if ($id == 0) {
+            $this->insert($data);
+        } else {
+            if ($this->getStorydetail($id)) {
+                $this->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Form id does not exist');
+            }
+        }
+    }
+
+    public function deleteStorydetail($id) {
+        $this->delete(array('id' => $id));
+    }
+
+}
