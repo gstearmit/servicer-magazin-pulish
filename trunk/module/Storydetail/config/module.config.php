ea3bccba3127261cee44e348462d0bef13935a3d
gstearmit@gmail.com <gstearmit@gmail.com@815b5aec-6364-ffa6-e4c1-eef17ad42f80>
2014-05-23 11:25:34 +0000

    Add Seach All Modul

--- trunk/module/Storydetail/config/module.config.php
+++ trunk/module/Storydetail/config/module.config.php
@@ -12,11 +12,10 @@ return array(
             'storydetail' => array(
                 'type'    => 'segment',
                 'options' => array(
-                  //  'route'    => '/storydetail[/][:controller][/][:action][/:id][/page/:page][/order_by/:order_by][/:order][/mz/:idmz]',
-					 'route'    => '/storydetail[/:action][/:id][/page/:page][/order_by/:order_by][/:order][/mz/:idmz]',
+					 'route'    => '/storydetail[/:action][/:id][/page/:page][/order_by/:order_by][/:order][/search_by/:search_by]',
                     'constraints' => array(
                     	//'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action' => '(?!\bpage\b)(?!\border_by\b)[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action' => '(?!\bpage\b)(?!\border_by\b)(?!\bsearch_by\b)[a-zA-Z][a-zA-Z0-9_-]*',
                         'id'     => '[0-9]+',
                         'page' => '[0-9]+',
                         'order_by' => '[a-zA-Z][a-zA-Z0-9_-]*',
