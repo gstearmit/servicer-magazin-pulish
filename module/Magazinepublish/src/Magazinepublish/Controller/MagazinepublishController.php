f02e6f21cad51f892d3e20d098f5f0603d2687f4
gstearmit@gmail.com <gstearmit@gmail.com@815b5aec-6364-ffa6-e4c1-eef17ad42f80>
2014-05-23 10:21:32 +0000

    fix all Seach

--- module/Magazinepublish/src/Magazinepublish/Controller/MagazinepublishController.php
+++ module/Magazinepublish/src/Magazinepublish/Controller/MagazinepublishController.php
@@ -7,6 +7,7 @@ use Zend\View\Model\ViewModel;
 use Magazinepublish\Model\Magazinepublish;
 use Magazinepublish\Form\MagazinepublishForm;
 use Magazinepublish\Form\MagazineForm;
+use Magazinepublish\Form\MagazinepublishSearchForm as SearchFromMagazinepublish ;
 
 use Mzimg\Model\Mzimg;
 
@@ -22,30 +23,86 @@ use Mzimg\Model\MzimgTable;
 
 class MagazinepublishController extends AbstractActionController {
 	protected $magazinepublishTable;
+	
+	public function searchAction()
+	{
+	
+		$request = $this->getRequest();
+	
+		$url = 'index';
+	
+		if ($request->isPost()) {
+			$formdata    = (array) $request->getPost();
+			$search_data = array();
+			foreach ($formdata as $key => $value) {
+				if ($key != 'submit') {
+					if (!empty($value)) {
+						$search_data[$key] = $value;
+					}
+				}
+			}
+			if (!empty($search_data)) {
+				$search_by = json_encode($search_data);
+				$url .= '/search_by/' . $search_by;
+			}
+		}
+		$this->redirect()->toUrl($url);
+	}
+	
+	
 	public function indexAction() {
 		// check login
 // 		if (! $this->zfcUserAuthentication ()->hasIdentity ()) {
 // 			return $this->redirect ()->toRoute ( 'zfcuser/login' );
 // 		} else {
 			
+			$searchform = new SearchFromMagazinepublish();
+			$searchform->get('submit')->setValue('Search');
+			
 			$select = new Select ();
 			
 			$order_by = $this->params ()->fromRoute ( 'order_by' ) ? $this->params ()->fromRoute ( 'order_by' ) : 'id';
 			$order = $this->params ()->fromRoute ( 'order' ) ? $this->params ()->fromRoute ( 'order' ) : Select::ORDER_DESCENDING;
 			$page = $this->params ()->fromRoute ( 'page' ) ? ( int ) $this->params ()->fromRoute ( 'page' ) : 1;
+			$search_by = $this->params()->fromRoute('search_by') ? $this->params()->fromRoute('search_by') : '';
+			$select->order($order_by . ' ' . $order);
+			$where    = new \Zend\Db\Sql\Where();
+			$formdata = array();
+			if (!empty($search_by)) {
+				$formdata = (array) json_decode($search_by);
+				if (!empty($formdata['descriptionkey'])) {
+					$where->addPredicate(
+							new \Zend\Db\Sql\Predicate\Like('descriptionkey', '%' . $formdata['descriptionkey'] . '%')
+					);
+				}
+				if (!empty($formdata['title'])) {
+					$where->addPredicate(
+							new \Zend\Db\Sql\Predicate\Like('title', '%' . $formdata['title'] . '%')
+					);
+				}
 			
-			$magazinepublishs = $this->getMagazinepublishTable ()->fetchAll ( $select->order ( $order_by . ' ' . $order ) );
+			}
+			if (!empty($where)) {
+				$select->where($where);
+			}
+			
+			$magazinepublishs = $this->getMagazinepublishTable ()->fetchAll ( $select) ;
 			$itemsPerPage = 10; // is Number record/page
-			
+			$totalRecord  = $magazinepublishs->count();
 			$magazinepublishs->current ();
 			$paginator = new Paginator ( new paginatorIterator ( $magazinepublishs ) );
 			$paginator->setCurrentPageNumber ( $page )->setItemCountPerPage ( $itemsPerPage )->setPageRange ( 4 ); // is number page want view
 			
 			return new ViewModel ( array (
+					'search_by'  => $search_by,
+					'order_by' => $order_by,
 					'order_by' => $order_by,
 					'order' => $order,
 					'page' => $page,
-					'paginator' => $paginator 
+					'paginator' => $paginator ,
+					'pageAction' => 'magazinepublishs ',
+					'form'       => $searchform,
+					'totalRecord' => $totalRecord,
 			) );
 		//} // login
 	}
---
+++ module/Magazinepublish/src/Magazinepublish/Form/MagazinepublishSearchForm.php
@@ -0,0 +1,42 @@
+<?php
+namespace Magazinepublish\Form;
+
+use Zend\Form\Form;
+use \Zend\Form\Element;
+
+class MagazinepublishSearchForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('magazinepublish');
+        $this->setAttribute('class', 'form-horizontal');
+        $this->setAttribute('method', 'post');
+
+
+        $title = new Element\Text('title');
+        $title->setLabel('Title')
+        ->setAttribute('class', 'required')
+        ->setAttribute('placeholder', 'Title');
+	
+        $description = new Element\Text('descriptionkey');
+        $description->setLabel('Description')
+                ->setAttribute('class', 'required')
+                ->setAttribute('placeholder', 'description');
+        
+
+
+        $submit = new Element\Submit('submit');
+        $submit->setValue('Search')
+                ->setAttribute('class', 'btn btn-primary');
+
+
+        $this->add($description);
+	    $this->add($title);
+	
+        $this->add($submit);
+
+    }
+}
+
+
+    
