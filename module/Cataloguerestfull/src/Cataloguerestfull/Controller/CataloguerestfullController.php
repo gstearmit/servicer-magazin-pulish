6ad523e7d549033aa9e5136c4a66791fabc1352a
gstearmit@gmail.com <gstearmit@gmail.com@815b5aec-6364-ffa6-e4c1-eef17ad42f80>
2014-05-26 07:01:14 +0000

    Add Modull    cataloguerestfull    cataloguerest

---
+++ module/Cataloguerestfull/src/Cataloguerestfull/Controller/CataloguerestfullController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Cataloguerestfull\Controller;
+
+use Zend\Mvc\Controller\AbstractRestfulController;
+
+
+use Catalogue\Model\Catalogue;          // <-- Add this import
+use Catalogue\Form\CatalogueForm;       // <-- Add this import
+use Catalogue\Model\CatalogueTable;     // <-- Add this import
+use Zend\View\Model\JsonModel;
+
+class CataloguerestfullController extends AbstractRestfulController
+{
+    protected $catalogueTable;
+
+    public function getList()
+    {
+    	$results = $this->getCatalogueTable()->fetchAll(); //ASC id
+    	//$results = $this->getCatalogueTable()->fetchAllOrderbyiddesc();
+        $data = array();
+        foreach($results as $result) {
+            $data[] = $result;
+        }
+
+        return new JsonModel(array(
+            'data' => $data,
+        ));
+    }
+
+    public function get($id)
+    {
+        $catalogue = $this->getCatalogueTable()->getCatalogue($id);
+       
+        return new JsonModel(array(
+            'data' => $catalogue,
+        ));
+    }
+
+    public function create($data)
+    {
+        $form = new CatalogueForm();
+        $catalogue = new Catalogue();
+        $form->setInputFilter($catalogue->getInputFilter());
+        $form->setData($data);
+        if ($form->isValid()) {
+            $catalogue->exchangeArray($form->getData());
+            $id = $this->getCatalogueTable()->saveCatalogue($catalogue);
+        }
+
+        return new JsonModel(array(
+            'data' => $this->get($id),
+        ));
+    }
+
+    public function update($id, $data)
+    {
+        $data['id'] = $id;
+        $catalogue = $this->getCatalogueTable()->getCatalogue($id);
+        $form  = new CatalogueForm();
+        $form->bind($catalogue);
+        $form->setInputFilter($catalogue->getInputFilter());
+        $form->setData($data);
+        if ($form->isValid()) {
+            $id = $this->getCatalogueTable()->saveCatalogue($form->getData());
+        }
+
+        return new JsonModel(array(
+            'data' => $this->get($id),
+        ));
+    }
+
+    public function delete($id)
+    {
+        $this->getCatalogueTable()->deleteCatalogue($id);
+
+        return new JsonModel(array(
+            'data' => 'deleted',
+        ));
+    }
+
+    public function getCatalogueTable()
+    {
+        if (!$this->catalogueTable) {
+            $sm = $this->getServiceLocator();
+            $this->catalogueTable = $sm->get('Catalogue\Model\CatalogueTable');
+        }
+        return $this->catalogueTable;
+    }
+}
