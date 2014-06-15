f905be23250f45aa4d5955ff156c8a2136cca835
gstearmit@gmail.com <gstearmit@gmail.com@815b5aec-6364-ffa6-e4c1-eef17ad42f80>
2014-06-15 04:55:58 +0000

    Rss get

---
+++ trunk/module/Rssget/config/module.config.php
@@ -0,0 +1,36 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Rssget\Controller\Rssget' => 'Rssget\Controller\RssgetController',
+        ),
+    ),
+    
+    'router' => array(
+        'routes' => array(
+            'rssget' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/rssget[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Rssget\Controller\Rssget',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Rssget' => __DIR__ . '/../view',
+        ),
+    		'template_map' => array(
+    				'paginator-Rssget' => __DIR__ . '/../view/layout/slidePaginator.phtml',
+    		),
+    ),
+);
---
+++ trunk/module/Rssget/src/Rssget/Controller/RssgetController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Rssget\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Rssget\Model\Rssget;
+use Rssget\Form\RssgetForm;
+use Zend\Paginator\Paginator;
+use Zend\Paginator\Adapter\Iterator as paginatorIterator;
+use Zend\Db\Sql\Select;
+
+// rss
+use Zend\Feed\Reader as feed;
+
+class RssgetController extends AbstractActionController {
+	protected $rssgetTable;
+	public function indexAction() {
+		
+		// if (!$this->zfcUserAuthentication()->hasIdentity()) {
+		// return $this->redirect()->toRoute('zfcuser/login');
+		// }
+		$select = new Select ();
+		$order_by = $this->params ()->fromRoute ( 'order_by' ) ? $this->params ()->fromRoute ( 'order_by' ) : 'id';
+		$order = $this->params ()->fromRoute ( 'order' ) ? $this->params ()->fromRoute ( 'order' ) : Select::ORDER_ASCENDING;
+		$page = $this->params ()->fromRoute ( 'page' ) ? ( int ) $this->params ()->fromRoute ( 'page' ) : 1;
+		
+		$rssgets = $this->getRssgetTable ()->fetchAll ( $select->order ( $order_by . ' ' . $order ) );
+		$itemsPerPage = 3;
+		
+		$rssgets->current ();
+		$paginator = new Paginator ( new paginatorIterator ( $rssgets ) );
+		$paginator->setCurrentPageNumber ( $page )->setItemCountPerPage ( $itemsPerPage )->setPageRange ( 4 );
+		
+		return new ViewModel ( array (
+				// 'rssgets' => $this->getRssgetTable()->fetchAll(),
+				'order_by' => $order_by,
+				'order' => $order,
+				'page' => $page,
+				'paginator' => $paginator 
+		) );
+	}
+	public function addAction() {
+		$form = new RssgetForm ();
+		$form->get ( 'submit' )->setAttribute ( 'value', 'Add' );
+		
+		$request = $this->getRequest ();
+		if ($request->isPost ()) {
+			$rssget = new Rssget ();
+			$form->setInputFilter ( $rssget->getInputFilter () );
+			$form->setData ( $request->getPost () );
+			if ($form->isValid ()) {
+				$rssget->exchangeArray ( $form->getData () );
+				$this->getRssgetTable ()->saveRssget ( $rssget );
+				
+				// Redirect to list of rssgets
+				return $this->redirect ()->toRoute ( 'rssget' );
+			}
+		}
+		
+		return array (
+				'form' => $form 
+		);
+	}
+	public function editAction() {
+		$id = ( int ) $this->params ( 'id' );
+		if (! $id) {
+			return $this->redirect ()->toRoute ( 'rssget', array (
+					'action' => 'add' 
+			) );
+		}
+		$rssget = $this->getRssgetTable ()->getRssget ( $id );
+		
+		$form = new RssgetForm ();
+		$form->bind ( $rssget );
+		$form->get ( 'submit' )->setAttribute ( 'value', 'Edit' );
+		
+		$request = $this->getRequest ();
+		if ($request->isPost ()) {
+			$form->setData ( $request->getPost () );
+			if ($form->isValid ()) {
+				$this->getRssgetTable ()->saveRssget ( $rssget );
+				
+				// Redirect to list of rssgets
+				return $this->redirect ()->toRoute ( 'rssget' );
+			}
+		}
+		
+		return array (
+				'id' => $id,
+				'form' => $form 
+		);
+	}
+	public function deleteAction() {
+		$id = ( int ) $this->params ( 'id' );
+		if (! $id) {
+			return $this->redirect ()->toRoute ( 'rssget' );
+		}
+		
+		$request = $this->getRequest ();
+		if ($request->isPost ()) {
+			$del = $request->getPost ()->get ( 'del', 'No' );
+			if ($del == 'Yes') {
+				$id = ( int ) $request->getPost ()->get ( 'id' );
+				$this->getRssgetTable ()->deleteRssget ( $id );
+			}
+			
+			// Redirect to list of Rssgets
+			return $this->redirect ()->toRoute ( 'rssget' );
+		}
+		
+		return array (
+				'id' => $id,
+				'rssget' => $this->getRssgetTable ()->getRssget ( $id ) 
+		);
+	}
+	public function rssgetAction() {
+		try {
+			
+			$rss = feed\Reader::import ( 'http://www.wdcdn.net/rss/presentation/library/client/skunkus/id/cc3d06c1cc3834464aef22836c55d13a' );
+		} catch ( feed\Exception\RuntimeException $e ) {
+			echo "error : " . $e->getMessage ();
+			exit ();
+		}
+		
+		$channel = array (
+				'title' => $rss->getTitle (),
+				'description' => $rss->getDescription (),
+				'link' => $rss->getLink (),
+				'items' => array () 
+		);
+		
+// 		echo 'rss</br>';
+// 		echo '<pre>';
+// 		print_r($rss);
+// 		echo '</pre>';
+		
+		foreach ( $rss as $item ) 
+		{
+
+			
+// 			foreach ($item->getMedia() as $res) {
+// 				$selectDataImg['url'] = $res['url'];
+// 			}
+			
+			$channel ['items'] [] = array (
+					'title' => $item->getTitle (),
+					'link' => $item->getLink (),
+					'description' => $item->getDescription () ,
+			        'image' => $item->getMedia()->url,
+						);
+		}
+		
+		return new ViewModel ( array (
+				'channel' => $channel 
+		) );
+	}
+	
+	
+	public function getMedia()
+	{
+		if (array_key_exists('media', $this->data)) {
+			return $this->data['media'];
+		}
+	
+		$media = null;
+	
+		if ($this->getType() == Reader\Reader::TYPE_RSS_20) {
+			$nodeList = $this->xpath->query($this->xpathQueryRss . '/media:thumbnail');
+	
+			if ($nodeList->length > 0) {
+				$media = new \stdClass();
+				$media->url    = $nodeList->item(0)->getAttribute('url');
+	
+			}
+		}
+		$this->data['media'] = $media;
+	
+		return $this->data['media'];
+	}
+	
+	public function getRssgetTable() {
+		if (! $this->rssgetTable) {
+			$sm = $this->getServiceLocator ();
+			$this->rssgetTable = $sm->get ( 'Rssget\Model\RssgetTable' );
+		}
+		return $this->rssgetTable;
+	}
+}
